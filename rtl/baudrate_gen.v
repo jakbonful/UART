f6c2f66e5d200ef96410a59d1fe3ad4bbc34793b
jakbonful <johnalexbonful@gmail.com>
2026-06-13 09:52:50 +0000

    stable structure

--- rtl/baudrate_gen.v
+++ rtl/baudrate_gen.v
@@ -1,30 +1,29 @@
 module baudrate_gen #(
-    parameter CLK_FREQ = 50_000_000, // 50 MHz;
-    parameter BAUD_RATE = 115200 // 115200 bps
+    parameter CLK_FREQ = 50_000_000,
+    parameter BAUD_RATE = 115200
 ) (
     input clk,
     input rst_n,
     output reg baud_tick
 );
 
-    localparam CLKS_PER_BIT = CLK_FREQ/BAUD_RATE;
+    localparam integer CLKS_PER_BIT = CLK_FREQ / BAUD_RATE;
+
     reg [$clog2(CLKS_PER_BIT)-1:0] counter;
 
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin // clears all inputs and outputs
-        counter <= 0;
-        baud_tick <= 0;
-    end  
-    else if (counter == CLKS_PER_BIT -1) begin
-        counter <= 0; // reset counter
-        baud_tick <= 1;
-    end 
-    else begin
-        counter <= counter + 1'b1;
-        baud_tick <= 0; // tick remains zero
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            counter <= 0;
+            baud_tick <= 0;
+        end
+        else if (counter == CLKS_PER_BIT - 1) begin
+            counter <= 0;
+            baud_tick <= 1'b1;
+        end
+        else begin
+            counter <= counter + 1'b1;
+            baud_tick <= 1'b0;
+        end
     end
-        
-end
-   
-    
+
 endmodule
